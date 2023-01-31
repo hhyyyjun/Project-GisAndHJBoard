1202eead0ad61c8ce348347f36a1f1e331b89494
hj <hj@DESKTOP-AS0B4U6>
2023-01-31 18:09:03 +0900

    주석 수정

--- mapstudy/src/main/java/com/lee/mapstudy/common/CustomMap.java
+++ mapstudy/src/main/java/com/lee/mapstudy/common/CustomMap.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 import org.springframework.jdbc.support.JdbcUtils;
 //mapper resultmap 생략
-
+//camel case 처리
 @SuppressWarnings("serial")
 public class CustomMap extends HashMap<String, Object> {
 
